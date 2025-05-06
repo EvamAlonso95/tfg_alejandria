7cbf5817d9d2e09c2511ed41bf93dcbd38b730a4
Eva Maria Alonso Medina <evam.alonso95@gmail.com>
2025-05-06 18:18:46 +0200

    feat: enhance book profile layout and author display

--- views/components/bookProfile.php
+++ views/components/bookProfile.php
@@ -4,10 +4,17 @@
  *  @var Book $book
  */
 ?>
-<div class="book-card">
-    <img src="<?= $book->getCoverImg() ?>" alt="Imagen del libro" class="book-cover img-fluid">
+<div class="book-card d-flex flex-column  mb-3">
+    <div class="me-3 container-img">
+        <img src="<?= $book->getCoverImg() ?>" alt="Imagen del libro" class="book-cover img-fluid">
+    </div>
     <h6>
         <a href="<?= base_url ?>book?bookId=<?= $book->getId() ?>"><?= $book->getTitle() ?></a>
     </h6>
-    <p><?= $book->getSynopsis() ?></p>
+    <?php foreach ($book->getAuthors() as $author): ?>
+        <p class="mb-1">
+            <a href="<?= base_url ?>author?authorId=<?= $author->getId() ?>"><?= $author->getName() ?></a>
+        </p>
+    <?php endforeach; ?>
+  
 </div>
