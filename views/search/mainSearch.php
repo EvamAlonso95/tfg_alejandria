4bfbaba322c817f5fd5e8bc357a36c8999eda7bd
Eva Maria Alonso Medina <evam.alonso95@gmail.com>
2025-05-25 20:47:39 +0200

    feat: enhance no results message in search; improve user feedback with alert and icon

--- views/search/mainSearch.php
+++ views/search/mainSearch.php
@@ -24,7 +24,15 @@
  		<div class="row mt-4">
  			<div class="col-lg-12">
  				<?php if (empty($books)) { ?>
- 					<h4 class="text-center no-results">No se encontraron resultados</h4>
+ 					<div class="alert alert-secondary text-center mt-4 py-4 no-books-message" role="alert">
+ 						<div class="icon-wrapper mb-3">
+ 							<svg xmlns="http://www.w3.org/2000/svg" height="40" width="40" viewBox="0 0 496 512" aria-hidden="true">
+ 								<path fill="#E07A5F" d="M248 8C111 8 0 119 0 256s111 248 248 248 248-111 248-248S385 8 248 8zm144 386.4V280c0-13.2-10.8-24-24-24s-24 10.8-24 24v151.4C315.5 447 282.8 456 248 456s-67.5-9-96-24.6V280c0-13.2-10.8-24-24-24s-24 10.8-24 24v114.4c-34.6-36-56-84.7-56-138.4 0-110.3 89.7-200 200-200s200 89.7 200 200c0 53.7-21.4 102.5-56 138.4zM205.8 234.5c4.4-2.4 6.9-7.4 6.1-12.4-4-25.2-34.2-42.1-59.8-42.1s-55.9 16.9-59.8 42.1c-.8 5 1.7 10 6.1 12.4 4.4 2.4 9.9 1.8 13.7-1.6l9.5-8.5c14.8-13.2 46.2-13.2 61 0l9.5 8.5c2.5 2.3 7.9 4.8 13.7 1.6zM344 180c-25.7 0-55.9 16.9-59.8 42.1-.8 5 1.7 10 6.1 12.4 4.5 2.4 9.9 1.8 13.7-1.6l9.5-8.5c14.8-13.2 46.2-13.2 61 0l9.5 8.5c2.5 2.2 8 4.7 13.7 1.6 4.4-2.4 6.9-7.4 6.1-12.4-3.9-25.2-34.1-42.1-59.8-42.1zm-96 92c-30.9 0-56 28.7-56 64s25.1 64 56 64 56-28.7 56-64-25.1-64-56-64z" />
+ 							</svg>
+ 							</svg>
+ 						</div>
+ 						<strong>¡Ups! Ni con lupa encontramos libros...</strong>
+ 					</div>
  				<?php } else { ?>
  					<div class="row gy-3">
  						<?php
@@ -40,27 +48,4 @@
  	<button onclick="topFunction()" id="myBtn" title="Go to top">Top</button>
  </main>
 
-
- <script>
- 	// Get the button:
- 	let mybutton = document.getElementById("myBtn");
-
- 	// When the user scrolls down 20px from the top of the document, show the button
- 	window.onscroll = function() {
- 		scrollFunction()
- 	};
-
- 	function scrollFunction() {
- 		if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
- 			mybutton.style.display = "block";
- 		} else {
- 			mybutton.style.display = "none";
- 		}
- 	}
-
- 	// When the user clicks on the button, scroll to the top of the document
- 	function topFunction() {
- 		document.body.scrollTop = 0; // For Safari
- 		document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
- 	}
- </script>
+<?php require 'views/components/scrollUp.php'; ?>
